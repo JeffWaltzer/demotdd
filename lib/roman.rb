acf8693faf229b75a0a40c8a7afb13030eaf9e29
Jeff Waltzer <jeffwaltzer@gmail.com>
2015-09-21 17:06:31 -0400

    Basics completed

--- lib/roman.rb
+++ lib/roman.rb
@@ -1,2 +1,27 @@
 class Roman
+
+  TRANSLATE = {
+      'I' => 1,
+      'V' => 5,
+      'X' => 10,
+      'D' => 500,
+      'L' => 50,
+      'C' => 100,
+      'M' => 1000
+
+  }
+
+  def self.convert(roman_string)
+    sum = 0
+    last = 9999999
+    roman_string.each_char do |char|
+      value = TRANSLATE[char]
+      sum+= value
+      if value > last
+        sum -= last * 2
+      end
+      last = value
+    end
+    sum
+  end
 end
--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -2,7 +2,27 @@ require 'rspec'
 require_relative '../lib/roman'
 
 describe 'first spec' do
-  it 'Calculates value of I' do
-    expect(Roman.convert('I')).to eq 1
+
+  {
+      'I' => 1,
+      'II' => 2,
+      'V' => 5,
+      'VI' => 6,
+      'IV' => 4,
+      'X' => 10,
+      'IX' => 9,
+      'XIV' => 14,
+      'L' => 50,
+      'C' => 100,
+      'D' => 500,
+      'M' => 1000,
+      'MCMLXXXIV'  => 1984
+  }.each do |test, expected|
+
+    it "Calculates value of #{test}" do
+      expect(Roman.convert(test)).to eq expected
+    end
+
   end
+
 end
